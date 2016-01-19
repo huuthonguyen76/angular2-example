06fecb73446ae50eb3aa059e08592ffb9cd3beb8
Nguyen Huu Tho <huu.thonguyen76@gmail.com>
2016-01-19 23:46:12 +0700

    + Move css to sass. + Use event emitter to emit event.

--- my-swipe/my-swipe.component.js
+++ my-swipe/my-swipe.component.js
@@ -1,6 +1,33 @@
 (function (application) {
+  (application.components || (application.components = {})).myButton = ng.core.Component({
+    selector: 'my-button',
+    inputs: ['emitter'],
+    template: `
+      <div *ngIf="show">
+        <button>My button</button>
+      </div>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.show = false;
+    },
+    ngAfterViewInit: function () {
+      var _this = this;
+      _this.emitter.subscribe(function(event) {
+        if (event == 'swipeleft') {
+          _this.show = false;
+          console.log('swipeleft');
+        } else {
+          _this.show = true;
+          console.log('swipeRight');
+        }
+      });
+    }
+  });
   (application.components || (application.components = {})).mySwipe = ng.core.Component({
     selector: 'my-swipe',
+    directives: [application.components.myButton],
     template: `
       <div class="swipe-container">
         <div class ="swipe-content">
@@ -10,43 +37,18 @@
           <ng-content select=".swipe-action-section"></ng-content>
         </div>
       </div>
+      <my-button [emitter]="emitter"></my-button>
     `,
-    styles: [`
-      .swipe-container{
-        width: 50%;
-        height: 50px;
-        margin-bottom: 10px;
-        position: relative;
-        display: inline-block;
-        overflow: hidden;
-      }
-      .swipe-content{
-        background-color: rebeccapurple;
-        color: white;
-        width: 100%;
-        height: 100%;
-        position: relative;
-        z-index: 100;
-        transition: transform 0.3s ease-out;
-      }
-      .swipe-actions{
-          text-align: right;
-        color: white;
-          background-color: orangered;
-        position: absolute;
-        top: 0px;
-        right: 0px;
-        width: 80%;
-        height: 100%;
-      }
-      .swiped {
-        transform: translateX(-80%);
-      }
-    `]
+    styleUrls: ['my-swipe.css']
   })
   .Class({
-    constructor: function() {},
+    constructor: function() {
+      var _this = this;
+      _this.emitter = new ng.core.EventEmitter();
+    },
     ngAfterViewInit: function () {
+      var _this = this;
+
       var elements = document.getElementsByClassName('swipe-content');
       var manager;
       for (var i = 0; i < elements.length; i++) {
@@ -57,14 +59,15 @@
         });
 
         manager.on('swipeleft', function (e) {
+          _this.emitter.next('swipeleft');
           e.target.className = 'swipe-content swiped';
         });
         manager.on('swiperight', function (e) {
+          _this.emitter.next('swiperight');
           e.target.className = 'swipe-content';
         });
       }
     }
   });
-
 })(window.application || (window.application = {}));
 
